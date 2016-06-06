72dc18d5b9f57d74cf2beaa4894e650f192cb35a
GTAWWEKID <katy@tsiserver.us>
2016-06-06 01:24:58 -0500

    Added 'W2U' Project

---
+++ W2U/w2u.c
@@ -0,0 +1,45 @@
+/**************************************************
+		W2U (Win to Unix/Linux)
+	Created by GTAWWEKID; NON-PROFIT USE!
+	=====================================
+	USE: w2u [Win32 CMD]
+	EX:  w2u cls
+	-------------------------------------
+	  This script takes in Win32 commands
+	and executes a linux equilavent. :D
+
+	ToDo:
+	~Add multiple options
+	~Create reverse command (U2W)
+	~Fix 2D CHAR ARRAY
+***************************************************/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	// cmd Win32 -- Unix
+	char *cmd[50][50] = {
+	{ "cls" , "clear" },
+	{ "ipconfig" , "ifconfig" },
+
+	//default fall
+	{ "?" , "clear"}
+	};
+
+	//Get COMMAND AND CONVERT; THAN OUTPUT.
+	if( argc == 2 ) {
+		int look = 1; int section = 0;
+		do{
+			//Loop through selections (BRUTE-FORCE)
+			if(*cmd[section][0] == *argv[1]){system(cmd[section][1]);look--;}
+			else if(cmd[section][0] == "?"){look--;}
+			else{section++;}
+
+		}while( look == 1);
+	}
+   	else {
+      		printf("USE: w2u [Win32 Command]\n");
+   	}
+	return 0;
+}
+
